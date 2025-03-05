e38290c8d9c04c713949234ffe385d46ffe0833c
dkgus <dkgus12dkgus@naver.com>
2025-03-05 17:43:52 +0900

    fix: 배포에러수정

--- src/app/hotel/DetailCard.tsx
+++ src/app/hotel/DetailCard.tsx
@@ -1,12 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useParams } from "next/navigation";
 import { getAxiosData } from "@/lib/axiosData";
 import { Badge } from "@/components/ui/badge";
